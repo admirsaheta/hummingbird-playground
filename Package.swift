56c1225f3c12995bff5294f7e2942a6f4c3d428e
admirsaheta <admirxsaheta@gmail.com>
2025-01-24 14:52:45 +0100

    update:bg-remover-meta

--- Package.swift
+++ Package.swift
@@ -4,29 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "TestApp",
+    name: "BackgroundRemover-Swift",
     platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17)],
     products: [
-        .executable(name: "TestApp", targets: ["TestApp"]),
+        .executable(name: "BackgroundRemover-Swift", targets: ["BackgroundRemover-Swift"]),
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
     ],
     targets: [
-        .executableTarget(name: "TestApp",
+        .executableTarget(name: "BackgroundRemover-Swift",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Hummingbird", package: "hummingbird"),
             ],
             path: "Sources/App"
-        ),
-        .testTarget(name: "TestAppTests",
-            dependencies: [
-                .byName(name: "TestApp"),
-                .product(name: "HummingbirdTesting", package: "hummingbird")
-            ],
-            path: "Tests/AppTests"
         )
     ]
 )
